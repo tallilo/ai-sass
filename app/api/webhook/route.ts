a27fee9318748124c19746e2084e0f66c6286d91
tallilo <tallilo5011323@gmail.com>
2023-11-10 11:04:06 +0200

    fixed

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,6 @@ import { stripe } from "@/lib/stripe";
 
 export async function POST(req: Request) {
   const body = await req.text();
-
   const signature = headers().get("Stripe-Signature") as string;
 
   let event: Stripe.Event;
@@ -28,21 +27,24 @@ export async function POST(req: Request) {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
+
     if (!session?.metadata?.userId) {
       return new NextResponse("User id is required", { status: 400 });
     }
+
     await prismadb.userSubscription.create({
       data: {
         userId: session?.metadata?.userId,
         stripeSubscriptionId: subscription.id,
         stripeCustomerId: subscription.customer as string,
-        stripePriceId: subscription.items.data[0].price.id as string,
+        stripePriceId: subscription.items.data[0].price.id,
         stripeCurrentPeriodEnd: new Date(
           subscription.current_period_end * 1000
         ),
       },
     });
   }
+
   if (event.type === "invoice.payment_succeeded") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
@@ -53,12 +55,13 @@ export async function POST(req: Request) {
         stripeSubscriptionId: subscription.id,
       },
       data: {
-        stripePriceId: subscription.items.data[0].price.id as string,
+        stripePriceId: subscription.items.data[0].price.id,
         stripeCurrentPeriodEnd: new Date(
           subscription.current_period_end * 1000
         ),
       },
     });
   }
+
   return new NextResponse(null, { status: 200 });
 }
