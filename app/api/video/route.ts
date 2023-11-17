52bcde6fc3d04e94d62665519932de743bcd7251
tallilo <tallilo5011323@gmail.com>
2023-11-18 00:10:53 +0200

    addint edge function

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,14 +1,14 @@
 import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Replicate from "replicate";
 import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
-
+export const runtime = "edge";
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { userId } = auth();
     const body = await req.json();
